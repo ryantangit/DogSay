94fe18072d4635d5b741ecc81f8ea1f36df07815
Ryan Tan <ryantan.mail@gmail.com>
2026-02-06 16:33:27 -0800

    Woof Woof

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 const DOG_ASCII: &str = 
 "   / \\ / \\_      
    (  @  #\\___    
@@ -5,8 +7,14 @@ const DOG_ASCII: &str =
   /   (_____/      
   /_____/";
 
+const MAX_LENGTH: i32 = 50;
 
 fn main() {
+    println!("MAX_LENGTH = {}", MAX_LENGTH);
+    let args: Vec<String> = env::args().collect();
+    for arg in args {
+        println!("{arg}")
+    }
     let parts = DOG_ASCII.split("\n");
     for part in parts {
         println!("{part}")
