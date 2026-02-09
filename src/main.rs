ec8003a28d2790168a1a8f7cd55a434558476e1d
Ryan Tan <ryantan.mail@gmail.com>
2026-02-08 17:37:43 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,19 @@ const MAX_LENGTH: i32 = 50;
 fn main() {
     println!("MAX_LENGTH = {}", MAX_LENGTH);
     let args: Vec<String> = env::args().collect();
-    for arg in args {
-        println!("{arg}")
+    let mut words: Vec<String> = Vec::new();
+    for idx in 1..args.len() {
+        let input = &args[idx];
+        words.push(input.to_string());
     }
+    
+    let mut lines: Vec<String> = Vec::new();
+    lines.push(String::new());
+    for word in words {
+        //Word length is less than the current cell
+        lines[0].push_str(&word);
+    }
+    println!("{:?}", lines);
     let parts = DOG_ASCII.split("\n");
     for part in parts {
         println!("{part}")
