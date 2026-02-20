44d627bb3649a75ea7834f9c1bf10e39ca6e3f5b
Ryan Tan <ryantan.mail@gmail.com>
2026-02-19 17:44:23 -0800

    back to working on rando stuff for funzies

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const DOG_ASCII: &str =
   /   (_____/      
   /_____/";
 
-const MAX_LENGTH: i32 = 50;
+const MAX_LENGTH: usize = 46;
 
 fn main() {
     println!("MAX_LENGTH = {}", MAX_LENGTH);
@@ -18,11 +18,24 @@ fn main() {
         words.push(input.to_string());
     }
     
-    let mut lines: Vec<String> = Vec::new();
-    lines.push(String::new());
+    let mut lines: Vec<Vec<String>> = Vec::new();
+    let mut line_idx: usize = 0;
+    lines.push(Vec::new());
     for word in words {
-        //Word length is less than the current cell
-        lines[0].push_str(&word);
+        if word.len() > MAX_LENGTH {
+            let mut temp: usize = 0;
+            while temp < word.len() {
+                lines.push(Vec::new());
+                line_idx += 1;
+                lines[line_idx].push(word[temp..(temp+MAX_LENGTH).min(word.len())].to_string());
+                temp += MAX_LENGTH;
+            }
+       } else if word.len() + lines[line_idx].len() > MAX_LENGTH {
+            line_idx += 1;
+            lines.push(Vec::new())
+        }
+        lines[line_idx].push_str(" ");
+        lines[line_idx].push_str(&word);
     }
     println!("{:?}", lines);
     let parts = DOG_ASCII.split("\n");
@@ -30,3 +43,5 @@ fn main() {
         println!("{part}")
     }
 }
+
+
